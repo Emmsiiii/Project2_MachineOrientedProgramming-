78512102a08ff39571615671c4a62c4ac28c7579
Setare Izadi <s232629@dtu.dk>
2024-04-03 20:32:28 +0200

    lavet subtask 1.1

--- Classes/main.c
+++ Classes/main.c
@@ -1,26 +1,27 @@
-// Created by Emma Rebner on 03/04/2024.
-//
-
 #include <stdio.h>
 #include <stdbool.h> // Include this for bool type if using C99 or later
+#include <string.h> // Include this for strcpy and other string functions
 
-// defining a struct to represent a playing card
-typedef struct {
-    char rank;       // Using chars like 'A', '2', ..., '10', 'J', 'Q', 'K'
-    char suit;       // Using chars like 'H', 'D', 'C', 'S' for hearts, diamonds, clubs, spades
-    bool isVisible;  // true for face up, false for face down
+
+// Define a struct to represent a playing card
+struct Card {
+    char rank[3];     // Enough space for '10' plus a null terminator
+    char suit;        // Using chars like 'H', 'D', 'C', 'S' for hearts, diamonds, clubs, spades
+    bool isVisible;   // true for face up, false for face down
 };
 
-
 int main() {
-        Card myCard;
+    struct Card myCard;
 
-        myCard.rank = 'A';       // Ace
-        myCard.suit = 'H';       // Hearts
-        myCard.isVisible = true; // The card is face up
+    // Example of setting a card's properties
+    // For a two-character rank like '10', we need to copy a string
+    strcpy(myCard.rank, "10");  // Use strcpy to safely copy the string into the rank array
+    myCard.suit = 'H';          // Hearts
+    myCard.isVisible = true;    // The card is face up
 
-        printf("Card: %s of %c\n", myCard.rank, myCard.suit);
-        printf("Visible: %s\n", myCard.isVisible ? "Yes" : "No");
+    // Print card information
+    printf("Card: %s of %c\n", myCard.rank, myCard.suit);
+    printf("Visible: %s\n", myCard.isVisible ? "Yes" : "No");
 
-        return 0;
-    }
+    return 0;
+}
